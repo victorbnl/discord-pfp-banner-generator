dc1947c9d126a914fa6fe6442277952c182ae8ff
Victor B <victor.bonnelle@protonmail.com>
2021-08-25 22:35:01 +0200

    Use double quotes in setup.py

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@
 from distutils.core import setup
 
 setup(
-    name='discord-pfp-banner-generator',
-    description='A script to generate a profile picture and a banner that show the same image on Discord',
-    author='Victor B',
-    author_email='victor.bonnelle@protonmail.com',
-    url='https://github.com/victorbnl/discord-pfp-banner-generator/',
+    name="discord-pfp-banner-generator",
+    description="A script to generate a profile picture and a banner that show the same image on Discord",
+    author="Victor B",
+    author_email="victor.bonnelle@protonmail.com",
+    url="https://github.com/victorbnl/discord-pfp-banner-generator/",
     packages=[
-        'discord_pfp_banner_generator'
+        "discord_pfp_banner_generator"
     ],
     entry_points={
-        'console_scripts': [
-            'discord_pfp_banner_generator = discord_pfp_banner_generator.__main__:cli'
+        "console_scripts": [
+            "discord_pfp_banner_generator = discord_pfp_banner_generator.__main__:cli"
         ]
     },
     include_package_data=True,
@@ -22,8 +22,8 @@ setup(
         "discord_pfp_banner_generator/positions.yml"
     ],
     install_requires=[
-        'pillow',
-        'requests',
-        'PyYAML'
+        "pillow",
+        "requests",
+        "PyYAML"
     ]
 )
